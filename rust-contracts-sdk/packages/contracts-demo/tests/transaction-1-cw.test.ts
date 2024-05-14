87947a91c8ef1ac035e2d38fb15b16222cfa3593
ducphamle2 <ducphamle212@gmail.com>
2024-05-14 13:08:05 -0700

    feat: init contracts demo & tests

---
+++ rust-contracts-sdk/packages/contracts-demo/tests/transaction-1-cw.test.ts
@@ -0,0 +1,30 @@
+/* eslint-disable node/no-missing-import */
+
+describe("Tree of Cells parser tests 1", () => {
+  beforeAll(async function () {});
+
+  it("Should throw an error when use wrong boc for parseCandidatesRootBlock", async () => {});
+
+  it("Should add validators from boc to candidatesForValidators", async () => {});
+
+  // TODO: onlyOwner test
+
+  it("Should set initial validators and its block's hash", async () => {});
+
+  it("Should add validators for update from boc to candidatesForValidators", async () => {});
+
+  it("Should throw an exception for set validators when signatures was not checked", async () => {});
+
+  // TODO: check signatures for wrong boc/fileHash/vdata
+  it("Should verify signatures", async () => {});
+
+  it("should update validators", async () => {});
+
+  it("verify-signature test", async () => {});
+
+  it("shard state test", async () => {});
+
+  it("shard block test", async () => {});
+
+  it("bridge contract reads data from transaction", async () => {});
+});
