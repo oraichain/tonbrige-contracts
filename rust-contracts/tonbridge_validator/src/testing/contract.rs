00ffe37e901e03f88ebd9f181c1964107cb56a9b
ducphamle2 <ducphamle212@gmail.com>
2024-06-24 11:54:51 -0700

    chore: add test cases for entrypoints

---
+++ rust-contracts/tonbridge_validator/src/testing/contract.rs
@@ -0,0 +1,162 @@
+#[cfg(test)]
+mod tests {
+
+    use cosmwasm_std::{
+        from_binary,
+        testing::{mock_dependencies, mock_env, mock_info},
+        to_binary, Addr, Empty, HexBinary, Uint128,
+    };
+    use cw20::{BalanceResponse, Cw20Coin};
+    use cw_controllers::AdminError;
+    use tonbridge_parser::types::ValidatorDescription;
+    use tonbridge_validator::msg::{ExecuteMsg, QueryMsg, UserFriendlyValidator};
+
+    use crate::{
+        contract::{execute, query},
+        error::ContractError,
+        state::{OWNER, VALIDATOR},
+        validator::Validator,
+    };
+
+    const BOCS: &str = include_str!("testdata/bocs.hex");
+
+    #[test]
+    fn test_prepare_new_key_block() {
+        let mut deps = mock_dependencies();
+        let boc = HexBinary::from_hex(BOCS).unwrap();
+
+        VALIDATOR
+            .save(deps.as_mut().storage, &Validator::default())
+            .unwrap();
+
+        execute(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("relayer", &vec![]),
+            ExecuteMsg::PrepareNewKeyBlock { keyblock_boc: boc },
+        )
+        .unwrap();
+
+        let validators_bin = query(
+            deps.as_ref(),
+            mock_env(),
+            QueryMsg::GetCandidatesForValidators {
+                start_after: None,
+                limit: Some(30),
+                order: None,
+            },
+        )
+        .unwrap();
+
+        let validators: Vec<UserFriendlyValidator> = from_binary(&validators_bin).unwrap();
+
+        // choose two random indexes for testing
+        assert_eq!(
+            validators
+                .iter()
+                .find(|val| val.pubkey.to_hex()
+                    == "89462f768d318759a230f72ef92bdbcd02a09c791d40e6a01a53f42409e248a1"
+                        .to_string())
+                .is_some(),
+            true,
+        );
+        assert_eq!(
+            validators
+                .iter()
+                .find(|val| val.pubkey.to_hex()
+                    == "76627b87a5717e9caab3a8044a8f75fd8da98b512c057e56defea91529f9b573"
+                        .to_string())
+                .is_some(),
+            true,
+        );
+        assert_eq!(validators.len(), 14usize);
+    }
+
+    #[test]
+    fn test_reset_validator_set() {
+        let mut deps = mock_dependencies();
+        let boc = HexBinary::from_hex(BOCS).unwrap();
+
+        VALIDATOR
+            .save(deps.as_mut().storage, &Validator::default())
+            .unwrap();
+
+        OWNER
+            .set(deps.as_mut(), Some(Addr::unchecked("admin")))
+            .unwrap();
+
+        // suite 1: should fail since not an admin
+        let err = execute(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("relayer", &vec![]),
+            ExecuteMsg::ResetValidatorSet { boc: boc.clone() },
+        )
+        .unwrap_err();
+        assert_eq!(
+            err.to_string(),
+            ContractError::AdminError(AdminError::NotAdmin {}).to_string()
+        );
+
+        execute(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("admin", &vec![]),
+            ExecuteMsg::ResetValidatorSet { boc },
+        )
+        .unwrap();
+
+        // query validator list, should return some validators
+        let validators: Vec<UserFriendlyValidator> = from_binary(
+            &query(
+                deps.as_ref(),
+                mock_env(),
+                QueryMsg::GetValidators {
+                    start_after: None,
+                    limit: Some(30),
+                    order: None,
+                },
+            )
+            .unwrap(),
+        )
+        .unwrap();
+
+        // choose two random indexes for testing
+        assert_eq!(
+            validators
+                .iter()
+                .find(|val| val.pubkey.to_hex()
+                    == "89462f768d318759a230f72ef92bdbcd02a09c791d40e6a01a53f42409e248a1"
+                        .to_string())
+                .is_some(),
+            true,
+        );
+        assert_eq!(
+            validators
+                .iter()
+                .find(|val| val.pubkey.to_hex()
+                    == "76627b87a5717e9caab3a8044a8f75fd8da98b512c057e56defea91529f9b573"
+                        .to_string())
+                .is_some(),
+            true,
+        );
+        assert_eq!(validators.len(), 14usize);
+
+        // after reset, the candidate list should be empty
+        let candidates: Vec<UserFriendlyValidator> = from_binary(
+            &query(
+                deps.as_ref(),
+                mock_env(),
+                QueryMsg::GetCandidatesForValidators {
+                    start_after: None,
+                    limit: Some(30),
+                    order: None,
+                },
+            )
+            .unwrap(),
+        )
+        .unwrap();
+
+        assert_eq!(candidates.len(), 0);
+    }
+}
