77504535e8e97f165f2dc36b9c2908ed48bdf96c
trung2891 <trungnv280201@gmail.com>
2024-07-03 00:48:31 +0700

    chore: update testcase build packet commitment

--- contracts/tonbridge_bridge/src/helper.rs
+++ contracts/tonbridge_bridge/src/helper.rs
@@ -1,8 +1,5 @@
-use cosmwasm_std::{StdResult, Uint128};
-use tonlib::{
-    address::TonAddress,
-    cell::{CellBuilder, TonCellError},
-};
+use cosmwasm_std::Uint128;
+use tonlib::{address::TonAddress, cell::CellBuilder};
 
 use crate::{
     bridge::{RECEIVE_PACKET_TIMEOUT_MAGIC_NUMBER, SEND_TO_TON_MAGIC_NUMBER},
@@ -53,12 +50,48 @@ pub fn build_receive_packet_timeout_commitment(seq: u64) -> Result<Vec<u8>, Cont
 
 #[cfg(test)]
 mod tests {
-    use super::is_expired;
+    use cosmwasm_std::Uint128;
+
+    use super::{
+        build_bridge_to_ton_commitment, build_receive_packet_timeout_commitment, is_expired,
+    };
 
     #[test]
     fn test_is_expired() {
         assert_eq!(is_expired(1, 2), false);
         assert_eq!(is_expired(2, 1), true);
         assert_eq!(is_expired(1, 1), false);
+    }
+
+    #[test]
+    fn test_build_receive_packet_timeout_commitment() {
+        let commitment = build_receive_packet_timeout_commitment(1).unwrap();
+        assert_eq!(
+            commitment,
+            vec![
+                31, 112, 103, 53, 231, 62, 248, 14, 0, 155, 123, 249, 55, 209, 63, 240, 63, 255,
+                84, 147, 234, 64, 167, 198, 41, 72, 6, 213, 27, 47, 165, 189
+            ]
+        )
+    }
+
+    #[test]
+    fn test_build_bridge_to_ton_commitment() {
+        let commitment = build_bridge_to_ton_commitment(
+            1,
+            1576711861,
+            "EQABEq658dLg1KxPhXZxj0vapZMNYevotqeINH786lpwwSnT",
+            "EQABEq658dLg1KxPhXZxj0vapZMNYevotqeINH786lpwwSnT",
+            Uint128::from(10000000000u128),
+            1719945916,
+        )
+        .unwrap();
+        assert_eq!(
+            commitment,
+            vec![
+                119, 193, 142, 10, 121, 185, 151, 96, 213, 92, 38, 8, 35, 164, 206, 127, 169, 152,
+                124, 138, 154, 53, 213, 232, 194, 103, 121, 126, 40, 150, 236, 133
+            ]
+        )
     }
 }
